dda855d7f887a38e1e324ce4791a827d1fddc9ed
alexr <fabioromulo19@gmail.com>
2025-11-28 16:38:27 +0000

    optimizei os imports

--- app/src/main/java/com/example/views/HistoricoScreen.kt
+++ app/src/main/java/com/example/views/HistoricoScreen.kt
@@ -15,25 +15,24 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.romus.MainActivity
-import com.example.romus.ui.theme.RomusTheme
 import com.example.romus.model.HistoryItem
-import androidx.compose.material3.ExperimentalMaterial3Api
+import com.example.romus.ui.theme.RomusTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
